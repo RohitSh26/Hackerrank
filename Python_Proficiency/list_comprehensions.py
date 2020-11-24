1bc23297b4190341c0ccbec50db8ae67e9bc16c8
Rohit Sharma <rohit.sharma1048@gmail.com>
2020-11-24 16:01:53 -0500

    problem statement and assumptions

---
+++ Python_Proficiency/list_comprehensions.py
@@ -0,0 +1,15 @@
+
+
+# input x, y, z - cuboid dimention
+# input n
+
+# problem
+# print list of all possible coordinates given by (i, j, k) on a 3D grid
+# where sum(i + j + j) != n
+
+# constraints
+# 0 <= i <= x;  0 <= j <= y;  0 <= k <= z
+# 
+
+# if x = 1, i = 0 and 1
+
