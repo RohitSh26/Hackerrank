e0f50121874502148a8234c26b36aac3a822cb5d
Rohit Sharma <rohit.sharma1048@gmail.com>
2020-11-24 16:21:56 -0500

    add find the runner up score initial

---
+++ Python_Proficiency/find_the_runnerUp_score.py
@@ -0,0 +1,19 @@
+'''
+problem statement - given score sheet, find runner-up score.
+
+Input:
+n scores, store scores in the list
+
+output:
+returns the second highest score
+'''
+
+def get_second_highest_score():
+    output = 0
+    
+    return output
+
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
